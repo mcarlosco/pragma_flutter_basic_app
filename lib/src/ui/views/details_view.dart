84ae6193b3dbd0d2ee4b45bd30ab7724937bb579
Carlos Murillo <m.carlos.co@gmail.com>
2024-10-18 15:14:54 -0500

    define widget "DetailsView", which shows the detail of a given job

---
+++ lib/src/ui/views/details_view.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../data/models/job.dart';
+
+final class DetailsView extends StatelessWidget {
+  final Job job;
+
+  const DetailsView(
+    this.job, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          job.title,
+          style: textTheme.titleLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+            letterSpacing: .25,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: Text(
+            job.team.name,
+            style: textTheme.titleMedium,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: job.countries
+                .map((country) => Padding(
+                      padding: const EdgeInsets.only(bottom: 2.5),
+                      child: Text(
+                        '${country.flag} ${country.name}',
+                        style: textTheme.bodySmall,
+                      ),
+                    ))
+                .toList(),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 15),
+          child: Text(job.description),
+        ),
+      ],
+    );
+  }
+}
