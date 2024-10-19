45b8bbe44768629db88a67fbf55039328012ab18
Carlos Murillo <m.carlos.co@gmail.com>
2024-10-19 15:17:17 -0500

    define repository "CountriesRepository" and method "get" which gives a constant set of countries

---
+++ lib/src/data/repositories/countries_repository.dart
@@ -0,0 +1,28 @@
+import '../models/country.dart';
+
+final class CountriesRepository {
+  const CountriesRepository();
+
+  Set<Country> get() => const {
+        Country(
+          name: 'Colombia',
+          flag: '🇨🇴',
+        ),
+        Country(
+          name: 'Perú',
+          flag: '🇵🇪',
+        ),
+        Country(
+          name: 'Panamá',
+          flag: '🇵🇦',
+        ),
+        Country(
+          name: 'Guatemala',
+          flag: '🇬🇹',
+        ),
+        Country(
+          name: 'México',
+          flag: '🇲🇽',
+        ),
+      };
+}
